e6392647c19e2c97c4b40502e93a31ac4e207607
sco. <77859647+scovings@users.noreply.github.com>
2025-03-19 21:57:12 +0000

    err

---
+++ script.js
@@ -0,0 +1,8 @@
+const words = 'ine one good real one not school set they state him skibidi toilet ohio rizz nataniel daniel sigma toilet ohio hippopotomous erm what the sigma i love niggers fuck niggers they state script style index java live outline timeline windows'.split();
+
+function newGame() {
+  document.getElementById('words').innerHTML = '';
+  for (let i = 0; i < 200; i++) {
+    
+  }
+}
